f1e7bf8da763affdb4c1b14b3a5db5c8fa9f2014
Navid Ahmed <Navid.Ahmed@inspectrealestate.com.au>
2022-06-13 22:07:33 +1000

    NavBarAttempt

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route } from "react-router-dom";
+import Layout from "./components/layout/Layout";
+import AllAuctions from "./pages/AllAuctionsPage";
+import AucationWatchlist from "./pages/AuctionWatchlist";
+import NewAuction from "./pages/NewAuction";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Layout>
+      <Routes>
+        <Route path="/" exact>
+          <AllAuctions />
+        </Route>
+        <Route path="/new-auction">
+          <NewAuction />
+        </Route>
+        <Route path="/watch-list">
+          <AucationWatchlist />
+        </Route>
+      </Routes>
+    </Layout>
   );
 }
 
